18dce8002a4ef8b4b2d7740b4529f77b00887172
alicia bentley <alicia.bentley@dlogin02.dogwood.wcoss2.ncep.noaa.gov>
2026-09-02 12:54:53 +0000

    Updated wget script to download sfc AIGEFS files from AWS

--- gather_data/wget_from_aws/wget_aigefs_sfc_from_aws.sh
+++ gather_data/wget_from_aws/wget_aigefs_sfc_from_aws.sh
@@ -7,8 +7,7 @@ BASE_URL="https://noaa-nws-graphcastgfs-pds.s3.amazonaws.com/EAGLE_ensemble"
 
 # List of forecast hours
 # seq -f "%03g" <start> <increment> <end>
-# FHOURS=($(seq -f "%03g" 0 6 192))
-FHOURS=($(seq -f "%03g" 0 6 12))
+FHOURS=($(seq -f "%03g" 0 6 12))   # 192
 
 # Loop through members mem000 to mem030
 for mem in $(seq -f "%03g" 0 30); do
@@ -16,7 +15,7 @@ for mem in $(seq -f "%03g" 0 30); do
     
     # Loop through forecast hours
     for fhr in "${FHOURS[@]}"; do
-        FILE_NAME="aigefs.t${CYCLE_HOUR}z.pres.f${fhr}.grib2"
+        FILE_NAME="aigefs.t${CYCLE_HOUR}z.sfc.f${fhr}.grib2"
         FILE_URL="${BASE_URL}/aigefs.${CYCLE_DATE}/${CYCLE_HOUR}/${MEMBER}/model/atmos/grib2/${FILE_NAME}"
         
         # Define output directory structure to keep files organized
